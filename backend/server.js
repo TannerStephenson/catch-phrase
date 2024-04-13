87351ae28a598ef79856460d6ac7deab91ccdae0
Tanner Stephenson <tstephenson2@ewu.edu>
2024-04-13 13:46:10 -0700

    variable changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const Player = require('./Player');
 
 
